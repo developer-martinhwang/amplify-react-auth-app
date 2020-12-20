505321efa7cc3f365fda29e8d8b371cfae9ce1a7
Martin Hwang <developer.martinhwang@gmail.com>
2020-12-19 23:30:16 -0400

    customize amplify sign up using Amplify UI Components
    https://docs.amplify.aws/ui/auth/authenticator/q/framework/react#sign-in

--- src/pages/Authenticator.js
+++ src/pages/Authenticator.js
@@ -11,19 +11,47 @@
  * - Dec 17, 2020, Martin Hwang <developer.martinhwang@gmail.com> : Created
  * - Dec 17, 2020, Martin Hwang : install material-ui/core and take amplify authenticator apart from App.js
  * and username is set up with email.
+ * - Dec 19, 2020, Martin Hwang : customize sign in using amplify UI Components 
  */
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 // material-ui
-import { Typography } from '@material-ui/core'
+import { Box } from '@material-ui/core'
 // aws amplify
-import {AmplifySignOut, AmplifyAuthenticator} from "@aws-amplify/ui-react";
+import { AmplifyAuthenticator, AmplifySignUp, AmplifySignOut, AmplifySignIn } from '@aws-amplify/ui-react'
+import { AuthState, onAuthUIStateChange} from '@aws-amplify/ui-components'
 function Authenticator() {
+    const [authState, setAuthState] = useState()
+    const [user, setUser] = useState()
+    useEffect(() => { 
+        return onAuthUIStateChange((nextAuthState, authData) => {
+        setAuthState(nextAuthState);
+        console.log(nextAuthState)
+        setUser(authData)})
+    }, [])
     return (
+        authState === AuthState.SignedIn && user ? (
         <Fragment>
-            <AmplifyAuthenticator usernameAlias="email"/>
+            <Box paddingBottom="0.5em">Hello, {user.attributes.email}</Box>
             <AmplifySignOut/>
-            <Typography component="h6" variant="h6"> You are signed in</Typography>
-        </Fragment>
+        </Fragment>) : (
+        <Fragment>
+            <AmplifyAuthenticator>
+                <AmplifySignIn
+                    headerText="Custom Sign In"
+                    slot="sign-in"
+                    submitButtonText="Custom SignIn"
+                ></AmplifySignIn>
+                <AmplifySignUp
+                    slot="sign-up"
+                    usernameAlias="email"
+                    formFields={[
+                        { type: "email" },
+                        { type: "password" },
+                        { type: "phone_number"}
+                    ]}
+                />
+            </AmplifyAuthenticator>
+        </Fragment>)
     )
 }
 export default Authenticator;
